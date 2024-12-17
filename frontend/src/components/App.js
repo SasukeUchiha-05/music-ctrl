907f42110fa54be2f7b91411d0e045e9c097ea31
Karthik MVS <vijaysai.uchiha@gmail.com>
2024-12-17 12:12:35 +0530

    Day-4 commit.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React , {Component} from 'react';
 import { render } from 'react-dom';
+import HomePage from './HomePage';
+
 
 
 export default class App extends Component{
@@ -9,7 +11,11 @@ export default class App extends Component{
 
     render()
     {
-        return <h1>Testing react code</h1> ;
+        return(
+            <div>
+             <HomePage/>
+            </div>
+        );
     }
 }
 

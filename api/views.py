8fdde53474526bb57c3313c31d2a21e50db3f138
Karthik MVS <vijaysai.uchiha@gmail.com>
2025-02-10 11:00:57 +0530

    Video-9 commit

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer , CreateRoomSerilizer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -76,4 +77,15 @@ class CreateRoomView(APIView):
             
             return Response(RoomSerializer(room).data,status = status.HTTP_201_CREATED)
             
-    
+   
+
+
+class UserInRoom(APIView):
+    def get (self , request , format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        data={
+            'code':self.request.session.get('room_code')
+        }
+        return JsonResponse(data,status = status.HTTP_200_OK) 
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,15 +14,73 @@ import {
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
   }
+
+  async componentDidMount() {
+    fetch("/api/user-in-room").then((response) =>
+      response.json().then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      })
+    );
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button
+              component={Link}
+              to="/join"
+              variant="contained"
+              color="primary"
+            >
+              Join a Room
+            </Button>
+            <Button
+              component={Link}
+              to="/create"
+              variant="contained"
+              color="secondary"
+            >
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Routes>
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
-          <Route exact path="/" element={<p> This is the Home Page </p>} />
-          <Route path="/room/:roomCode" Component={Room}/>
+          {/* <Route exact path="/"  element={this.renderHomePage()} /> */}
+          <Route
+            exact
+            path="/"
+            element={
+              this.state.roomCode ? (
+                <Navigate replace to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              )
+            }
+          />
+          <Route path="/room/:roomCode" Component={Room} />
         </Routes>
       </Router>
     );
